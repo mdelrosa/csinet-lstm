ac5c5d0dbc62debd4a03e2b6f2ebf1513918d2cc
Mason del Rosario <mason.delro@gmail.com>
2021-01-31 14:59:05 -0800

    Add bash script for queuing training runs (tensorflow).

---
+++ csinet-lstm/csinet_train.sh
@@ -0,0 +1,5 @@
+python csinet_train.py -d 0 -g 0 -l csinet -r 512 -e indoor -ep 1000
+python csinet_train.py -d 0 -g 0 -l csinet -r 256 -e indoor -ep 1000
+python csinet_train.py -d 0 -g 0 -l csinet -r 128 -e indoor -ep 1000
+python csinet_train.py -d 0 -g 0 -l csinet -r 64 -e indoor -ep 1000
+python csinet_train.py -d 0 -g 0 -l csinet -r 32 -e indoor -ep 1000
