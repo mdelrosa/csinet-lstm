a26906139b7f27bb7bbc76811ba7101cc19f05ec
Mason del Rosario <mason.delro@gmail.com>
2021-02-01 09:15:18 -0800

    Add eval only mode via opt.train_argv

--- torch/csinet_torch.py
+++ torch/csinet_torch.py
@@ -107,7 +107,7 @@ if __name__ == "__main__":
     from utils.parsing import str2bool
     from utils.timing import Timer
     from utils.unpack_json import get_keys_from_json
-    from utils.trainer import fit, score, save_predictions, save_checkpoint_history
+    from utils.trainer import fit, score, save_predictions, save_checkpoint_history, load_checkpoint_history
 
     # set up timers
     timers = {
@@ -124,7 +124,7 @@ if __name__ == "__main__":
     parser.add_argument("-l", "--dir", type=str, default=None, help="subdirectory for saving model, checkpoint, history")
     parser.add_argument("-e", "--env", type=str, default="indoor", help="environment (either indoor or outdoor)")
     parser.add_argument("-ep", "--epochs", type=int, default=10, help="number of epochs to train for")
-    parser.add_argument("-tr", "--train_argv", type=int, default=1, help="flag for toggling training")
+    parser.add_argument("-tr", "--train_argv", type=str2bool, default=True, help="flag for toggling training")
     parser.add_argument("-t", "--n_truncate", type=int, default=32, help="value to truncate to along delay axis.")
     parser.add_argument("-ts", "--timeslot", type=int, default=0, help="timeslot which we are training (0-indexed).")
     parser.add_argument("-r", "--rate", type=int, default=512, help="number of elements in latent code (i.e., encoding rate)")
@@ -146,7 +146,7 @@ if __name__ == "__main__":
 #         json_config = "../config/csinet-pro-quadriga-indoor0001-sph.json" if opt.env == "indoor" else "../config/csinet-pro-quadriga-outdoor300-sph.json"
 
 #     model_dir, norm_range, minmax_file, dataset_spec, diff_spec, batch_num, lrs, batch_sizes, network_name, T, data_format = get_keys_from_json(json_config, keys=['model_dir','norm_range','minmax_file','dataset_spec', 'diff_spec', 'batch_num', 'lrs', 'batch_sizes', 'network_name', 'T', 'df'])
-    dataset_spec, minmax_file, img_channels, data_format, norm_range, T, network_name, model_dir, n_delay, lr, batch_size = get_keys_from_json(json_config, keys=["dataset_spec", "minmax_file", "img_channels", "df", "norm_range", "T", "network_name", "model_dir", "n_delay", "learning_rate", "batch_size"])
+    dataset_spec, minmax_file, img_channels, data_format, norm_range, T, network_name, model_dir, n_delay, lr, batch_size, diff_spec = get_keys_from_json(json_config, keys=["dataset_spec", "minmax_file", "img_channels", "df", "norm_range", "T", "network_name", "model_dir", "n_delay", "learning_rate", "batch_size", "diff_spec"])
 #     lr = lrs[0]
 #     batch_size = batch_sizes[0]
     aux_bool_list = get_keys_from_json(json_config, keys=["aux_bool"], is_bool=True)
@@ -161,27 +161,29 @@ if __name__ == "__main__":
     # load all data splits
 
     data_train, data_val = dataset_pipeline_col(opt.debug_flag, opt.aux_bool, dataset_spec, opt.aux_size, T = T, img_channels = input_dim[0], img_height = input_dim[1], img_width = input_dim[2], data_format = data_format, train_argv = opt.train_argv)
-    aux_train, x_train = data_train
-    aux_val, x_val = data_val
 
-    # loading directly from unnormalized data; normalize data
-    x_train = renorm_H4(x_train,minmax_file)
+    aux_val, x_val = data_val
     x_val = renorm_H4(x_val,minmax_file)
-    print('-> post-renorm: x_train range is from {} to {}'.format(np.min(x_train),np.max(x_train)))
     print('-> post-renorm: x_val range is from {} to {}'.format(np.min(x_val),np.max(x_val)))
+    valid_dataset = torch.utils.data.TensorDataset(torch.from_numpy(aux_val).float().to(device), torch.from_numpy(x_val).to(device))
+
+    if opt.train_argv:
+        aux_train, x_train = data_train
+        x_train = renorm_H4(x_train,minmax_file)
+        print('-> post-renorm: x_train range is from {} to {}'.format(np.min(x_train),np.max(x_train)))
+        train_dataset = torch.utils.data.TensorDataset(torch.from_numpy(aux_train).float().to(device), torch.from_numpy(x_train).to(device))
 
     model_dir += "/" + opt.env
     if opt.dir != None:
         model_dir += "/" + opt.dir
 
-    train_dataset = torch.utils.data.TensorDataset(torch.from_numpy(aux_train).float().to(device), torch.from_numpy(x_train).to(device))
-    valid_dataset = torch.utils.data.TensorDataset(torch.from_numpy(aux_val).float().to(device), torch.from_numpy(x_val).to(device))
-
-    cr_list = [512, 256, 128, 64, 32] # rates for different compression ratios
+    cr_list = [512, 128, 64, 32] # rates for different compression ratios
+    # cr_list = [256, 128, 64, 32] # rates for different compression ratios
     for cr in cr_list:
-        train_ldr = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
         valid_ldr = torch.utils.data.DataLoader(valid_dataset, batch_size=batch_size)
-        # train_ldr = torch.utils.data.DataLoader((torch.from_numpy(aux_train).to(device), torch.from_numpy(x_train).to(device)), batch_size=batch_size, shuffle=True) valid_ldr = torch.utils.data.DataLoader((torch.from_numpy(aux_val).to(device), torch.from_numpy(x_val).to(device)), batch_size=batch_size)
+        if opt.train_argv:
+            train_ldr = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
         encoder = Encoder(input_dim[0], input_dim[1], opt.n_truncate, cr)
         decoder = Decoder(input_dim[0], input_dim[1], opt.n_truncate, cr)
@@ -190,15 +192,20 @@ if __name__ == "__main__":
         pickle_dir = f"{model_dir}/cr{cr}/t1"
         print(f"--- pickle_dir is {pickle_dir} ---")
 
-        model, checkpoint, history, optimizer, timers = fit(csinet_pro,
-                                                            train_ldr,
-                                                            valid_ldr,
-                                                            batch_num,
-                                                            epochs=epochs,
-                                                            timers=timers,
-                                                            json_config=json_config,
-                                                            debug_flag=opt.debug_flag,
-                                                            pickle_dir=pickle_dir)
+        if opt.train_argv:
+            print(f"--- Fitting on training set ({x_train.shape[0]} batches) ---")
+            model, checkpoint, history, optimizer, timers = fit(csinet_pro,
+                                                                train_ldr,
+                                                                valid_ldr,
+                                                                batch_num,
+                                                                epochs=epochs,
+                                                                timers=timers,
+                                                                json_config=json_config,
+                                                                debug_flag=opt.debug_flag,
+                                                                pickle_dir=pickle_dir)
+        else:                                                            
+            print(f"--- Loading model, checkpoint, history, optimizer from {model_dir} ---")
+            model, checkpoint, history, optimizer = load_checkpoint_history(pickle_dir, csinet_pro, network_name=network_name)
 
         checkpoint = score(csinet_pro,
                         valid_ldr,
@@ -210,15 +217,16 @@ if __name__ == "__main__":
                         timers=timers,
                         json_config=json_config,
                         debug_flag=opt.debug_flag,
-                        str_mod=f"CsiNetPro CR={cr}"
+                        str_mod=f"CsiNet CR={cr} - {opt.env} -",
+                        diff_spec=diff_spec
                         )
 
         if not opt.debug_flag:                
 
-            del train_ldr
+            # del train_ldr
             # train_ldr = torch.utils.data.DataLoader((torch.from_numpy(aux_train).to(device), torch.from_numpy(x_train).to(device)), batch_size=batch_size)
-            train_ldr = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size)
-            save_predictions(csinet_pro, train_ldr, data_train, optimizer, timers, json_config=json_config, dir=pickle_dir, split="train")
-            save_predictions(csinet_pro, valid_ldr, data_test, optimizer, timers, json_config=json_config, dir=pickle_dir, split="valid")
+            # train_ldr = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size)
+            # save_predictions(csinet_pro, train_ldr, data_train, optimizer, timers, json_config=json_config, dir=pickle_dir, split="train")
+            # save_predictions(csinet_pro, valid_ldr, data_test, optimizer, timers, json_config=json_config, dir=pickle_dir, split="valid")
             save_checkpoint_history(checkpoint, history, optimizer, dir=pickle_dir, network_name=network_name)
-            del train_ldr, valid_ldr
+            # del train_ldr, valid_ldr
