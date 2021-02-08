c5df0b4ccee5ae9d695c6ef352c7d2d926070b0a
Mason del Rosario <mason.delro@gmail.com>
2021-02-08 09:54:51 -0500

    sh file for xsede

---
+++ csinet-lstm/csinet_train.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+
+#SBATCH
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=mdelrosa@ucdavis.edu
+#SBATCH -p GPU-AI 
+#SBATCH --job-name=cr512_out
+#SBATCH --time=0-12
+#SBATCH --gres=gpu:volta16:1
+#SBATCH --mem=64000 # memory required by job
+
+source $HOME/.bashrc 
+
+python csinet_train.py -d 0 -e outdoor -r 512 -l csinet -ep 10
+
+#wait
+~
+~
+~
+~
+~
+~
+~
+~
+
